4510364ca6e07ecea7392d69f5c34682d832dd71
Eduardo Frazão <eduardo_frazao@live.com>
2018-02-22 19:58:47 -0300

    Update chk_dpl.py

--- Check_duplicates/chk_dpl.py
+++ Check_duplicates/chk_dpl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2017 Eduardo Frazão ( https://github.com/fr4z40 )
+# Copyright 2018 Eduardo Frazão ( https://github.com/fr4z40 )
 #
 #   Licensed under the MIT License;
 #   you may not use this file except in compliance with the License.
@@ -14,6 +14,10 @@ from os import walk, stat
 from subprocess import check_output
 from argparse import ArgumentParser, RawTextHelpFormatter
 
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
+cpu_count = int(cpu_count())
+
 
 
 def md5sum(file_path):
@@ -25,36 +29,80 @@ def md5sum(file_path):
 
 
 
+def do_chk(fl_pth):
+    md5_rst = md5sum(fl_pth)
+    if md5_rst != None:
+        ctime = stat(fl_pth).st_ctime
+    else:
+        ctime = 0
+        fl_pth = (bytes(fl_pth, 'utf-8', 'ignore')).decode()
+    rst = {'ctime':ctime, 'path':fl_pth, 'md5':md5_rst}
+    return(rst)
+
+
+
+
+
+
 if __name__ == '__main__':
 
     parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+
     parser.add_argument('path', help='Folder Path', type=str)
+
     parser.add_argument('-d', '--detailed',
         help='0 or 1, if seted as 1, will be more verbose, default is 0', type=int)
+
+    parser.add_argument('-c', '--cores',
+        help='number of cores to use, by default its "half".\n* Can be a number: 1...N\n\tscript.py -c 8 /full/path/folder/\n* Or text: half / full / single\n\tscript.py -c half /full/path/folder/', type=str)
+
     args = parser.parse_args()
+
+    if args.cores:
+        if args.cores.isnumeric():
+            core_value = int(args.cores)
+            if (core_value > cpu_count) or (core_value == 0):
+                print("You can't set more cores than you have or set 0")
+                quit()
+        else:
+            core_value = args.cores.lower()
+            if core_value == "single":
+                core_value = 1
+            elif core_value == "half":
+                core_value = int(cpu_count/2)
+            elif core_value == "full":
+                core_value = cpu_count
+            else:
+                quit()
+    else:
+        core_value = int(cpu_count/2)
+
+    e = ProcessPoolExecutor(core_value)
+
 
 
     fd_pth = args.path
     files = {}
 
+    paths = []
     for dr in walk(fd_pth):
         for fl in dr[2]:
             fl_path = (('%s/%s' % (dr[0], fl)).replace('//', '/')).strip()
-            md5_rst = md5sum(fl_path)
-            if md5_rst != None:
-                ctime = stat(fl_path).st_ctime
-            else:
-                ctime = 0
-                fl_path = (bytes(fl_path, 'utf-8', 'ignore')).decode()
-            if md5_rst not in files:
-                files[md5_rst] = [[ctime, fl_path]]
-            else:
-                files[md5_rst].append([ctime, fl_path])
+            paths.append(fl_path)
 
+    #######################################
+
+    map_rst = (list(e.map(do_chk, paths)))
+    for item_file in map_rst:
+        if item_file['md5'] not in files:
+            files[item_file['md5']] = [[item_file['ctime'], item_file['path']]]
+        else:
+            files[item_file['md5']].append([item_file['ctime'], item_file['path']])
+
+    #######################################
 
     if args.detailed == 1:
         out_p = []
-
 
     for key in files.keys():
         if args.detailed == 1:
@@ -73,4 +121,5 @@ if __name__ == '__main__':
         out_p = sorted(set(out_p))
         for item in out_p:
             print('%s|%s|%s' % (item[2], str(item[1]), item[0]))
+
 
